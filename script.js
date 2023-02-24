dfa89e550c16592e4e8e436495b80f7c5158c4f5
HenryLampoglio <henrylampoglio@hotmail.com>
2023-02-24 18:29:33 -0300

    final version calculator

--- script.js
+++ script.js
@@ -7,11 +7,39 @@ const addNumbers = (value) =>
    currentResultText.innerText += value;
 }
 
-const calculate = () =>{
+const calculate = () =>
+{
     let CalculatedResult = document.querySelector(".current-result").textContent;
     previousResultText.innerText = CalculatedResult;    
     
     currentResultText.innerText = eval(CalculatedResult);;
+}
+
+const squaredNumber = () =>
+{
+    let squaredNumber = document.querySelector(".current-result").textContent;
+    previousResultText.innerText = squaredNumber
+
+    currentResultText.innerText = (parseFloat(squaredNumber) ** 2)
+    
+}
+
+const eraseCurrentResult = () =>
+{
+    currentResultText.innerText = "";
+}
+
+const eraseAllResult = () =>
+{
+    previousResultText.innerText = "";
+    currentResultText.innerText = "";
+}
+
+const eraseLastNumber = () =>
+{
+    let result = document.querySelector(".current-result").textContent;
+    let erasedResult = result.slice(0, -1);
+    currentResultText.innerText = erasedResult;
 }
 
 buttons.forEach((btn) =>
@@ -24,8 +52,23 @@ buttons.forEach((btn) =>
            addNumbers(value)
         
         }
-        else if(value === "="){
+        else if(value === "=")
+        {
             calculate()
+        }
+        else if(value === "x²")
+        {
+            squaredNumber();
+        }
+        else if (value === "CE")
+        {
+            eraseCurrentResult();
+        }
+        else if(value === "C"){
+            eraseAllResult();
+        }
+        else if(value === "DEL"){
+           eraseLastNumber();
         }
     })
 })
